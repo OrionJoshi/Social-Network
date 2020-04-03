14842a4a291bf6ecb4a1ed40d0da26445d32a8f1
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-03 01:28:25 -0700

    Data taken from signup form

---
+++ insert_user.php
@@ -0,0 +1,26 @@
+<?php
+    include('includes/connection.php');
+
+    if(isset($_POST['sign_up'])){
+        $first_name = htmlentities(mysqli_real_escape_string($conn,$_POST['first_name']));
+        $last_name = htmlentities(mysqli_real_escape_string($conn,$_POST['last_name']));
+        $pass = htmlentities(mysqli_real_escape_string($conn,$_POST['u_pass']));
+        $email = htmlentities(mysqli_real_escape_string($conn,$_POST['u_email']));
+        $country = htmlentities(mysqli_real_escape_string($conn,$_POST['u_country']));
+        $gender = htmlentities(mysqli_real_escape_string($conn,$_POST['u_gender']));
+        $birthday = htmlentities(mysqli_real_escape_string($conn,$_POST['u_birthday']));
+        $status = "Verified";
+        $posts = "no";
+        $newgid = sprintf('%05d',rand(0,999999));
+        $username = strtolower($first_name."_".$last_name."_". $newgid);
+        $check_username_query = "SELECT user_name FROM users WHERE user_emial='$email'";
+        $run_username = mysqli_query($con,$check_username_query);
+
+        if(strlen($pass)<9){
+            echo "<script>alert('Password Should be minimun 9 character!');</script>";
+            exit();
+        }
+
+    }
+
+?>
