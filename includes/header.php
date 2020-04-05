3956b13cfdfc661d2f1d06bb85420866fc3c046d
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-05 10:22:21 -0700

    nav bar added

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php
+        include("includes/connection.php");
+?>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" aria-expanded="false"></button>
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <a href="home.php" class="navbar-brand">Social Network</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                
+            </ul>
+        </div>
+    </div>
+
+</nav>
