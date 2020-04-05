d79722148102a63e3f2464e507ca139683610e0a
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-05 10:40:51 -0700

    fetching user data from database

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,29 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                
+                <?php
+                    $user = $_SESSION['user_email'];
+
+                    $get_user = "SELECT * FROM users WHERE user_email='$user'";
+                    $run_user = mysqli_query($con,$get_user);
+                    $row = mysqli_fetch_array($run_user);
+
+                    $user_id = $row['user_id'];
+                    $user_name = $row['user_name'];
+                    $first_name =$row['f_name'];
+                    $last_name = $row['l_name'];
+                    $describe_user = $row['describe_user'];
+                    $relationship_status = $row['relationship'];
+                    $user_pass = $row['user_pass'];
+                    $user_email = $row['user_email'];
+                    $user_country = $row['user_country'];
+                    $user_gender = $row['user_gender'];
+                    $user_birthday = $row['user_birthday'];
+                    $user_image = $row['user_image'];
+                    $user_cover = $row['user_cover'];
+                    $register_date = $row['user_reg_date'];
+                    $recovery_account =$row['recovery_account'];
+                ?>
             </ul>
         </div>
     </div>
