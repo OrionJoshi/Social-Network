2d6a5aabfcd24d80b46c180f2bcb3eab129a702f
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-06 05:41:57 -0700

    tilte added

--- profile.php
+++ profile.php
@@ -9,9 +9,17 @@
 ?>
 <html lang="en">
 <head>
+    <?php
+        $user = $_SESSION['user_email'];
+
+        $get_user = "SELECT * FROM users WHERE user_email='$user'";
+        $run_user = mysqli_query($con,$get_user);
+        $row = mysqli_fetch_array($run_user);
+        $user_name = $row['user_name'];
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo "$user_name";?>t</title>
 </head>
 <body>
     
