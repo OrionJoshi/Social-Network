2c9f782e9875f7aa78203c8346230852fa8f0fa1
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-06 05:15:03 -0700

    nav bar added

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <?php
-        include("includes/connection.php");
+    
+    include("connection.php");
 ?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
@@ -14,6 +15,7 @@
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <?php
+                    session_start();
                     $user = $_SESSION['user_email'];
 
                     $get_user = "SELECT * FROM users WHERE user_email='$user'";
@@ -35,7 +37,45 @@
                     $user_cover = $row['user_cover'];
                     $register_date = $row['user_reg_date'];
                     $recovery_account =$row['recovery_account'];
+
+                    $user_posts = "SELECT * FROM posts WHERE user_id = '$user_id'";
+                    $run_posts = mysqli_query($con,$user_posts);
+                    $posts = mysqli_num_rows($run_posts);
                 ?>
+                <li><a href='profile.php?<?php echo "u_id=$user_id"?>'></a><?php echo $first_name;?></li>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="home.php">find people</a></li>
+                <li><a href="messages.php?u_id=new">Messages</a></li>
+                <?php
+                    echo "
+                        <li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><span class=></span></a>
+                            <ul class= 'downdown-menu'>
+                                <li>
+                                    <a href='my_post.php?u_id=$user_id'>My Posts <span class='badge badge-secondary'>$posts</span></a>
+                                </li>
+                                <li>
+                                    <a href='edit_profile.php?u_id=$user_id'>Edit Account</a>
+                                </li>
+                                <li role='separator' class='divider'></li>
+                                <li>
+                                    <a href='logout.php'>Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    
+                    ";
+                ?>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <form action="results.php" method="get" class=" navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="user_query" placeholder="Search">
+                            <button type="submit" class="btn btn-info" name="search">Search</button>
+                        </div>
+                    </form>
+                </li>
             </ul>
         </div>
     </div>
