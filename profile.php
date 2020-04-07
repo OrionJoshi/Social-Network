dd688023a43a536f8de39bfcf697a46e5ee0c585
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-07 01:47:43 -0700

    css link added

--- profile.php
+++ profile.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="home_style2.css">
 </head>
 <body>
     <div class="row">
