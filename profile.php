f6d8ee362cad34e7504a3ca0a854c09a2ebb4ec1
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-07 01:21:28 -0700

    update button added

--- profile.php
+++ profile.php
@@ -19,15 +19,9 @@
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-    <title><?php echo "$user_name";?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </head>
 <body>
@@ -43,6 +37,14 @@
                             <ul class= 'nav pull-left' style='position:absolute;top:10px;left:40px;'>
                                 <li class='dropdown'>
                                     <button class='dropdown-toggle btn btn-default' data-toggle='dropdown'>Change Cover</button>
+                                    <div class='dropdown-menu'>
+                                        <center>
+                                        <label class='btn btn-info'>Select Cover
+                                        <input type='file' name='u_cover' size='60' />
+                                        </label><br><br>
+                                        <button name='submit' class='btn btn-info'>Update Cover</button>
+                                        </center>
+                                    </div>
                                 </li>
                             </ul>
                         </form>
