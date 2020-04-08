a5dd8c25f084b221f01111500e3c6fa2e8f67030
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-08 08:17:29 -0700

    post display php code added

--- functions/functions.php
+++ functions/functions.php
@@ -70,5 +70,67 @@
             }
         }
     }
+    function get_posts(){
+        global $con;
+        $per_page = 4;
 
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+        }else{
+            $page=1;
+        }
+
+        $start_from = ($page-1)*$per_page;
+
+        $get_posts = "SELECT * FROM posts ORDER BY 1 DESC LIMIT $start_from, $per_page";
+        $run_posts = mysqli_query($con,$get_posts);
+
+        while($row_posts = mysqli_fetch_array($run_posts)){
+            $post_id = $row_posts['post_id'];
+            $user_id = $row_posts['user_id'];
+            $content = substr($row_posts['post_content'],0,40);
+            $upload_image = $row_posts['upload_image'];
+            $post_date = $row_posts['post_date'];
+
+            $user = "SELECT * FROM users WHERE user_id = '$user_id' AND posts='yes'";
+            $run_user = mysqli_query($con,$user);
+            $row_user = mysqli_fetch_array($run_user);
+
+            $user_name = $row_user['user_name'];
+            $user_image = $row_user['user_image'];
+
+            //now displaying the post
+
+            if($content=="NO" && strlen($upload_image)>=1){
+                echo"
+                    <div class='row'>
+                        <div class='col-sm-3'>
+                        </div>
+                        <div id='posts' class='col-sm-6'>
+                            <div class='row'>
+                                <div class='col-sm-2'>
+                                    <p><img src='$user_image' class = 'img-circle' width='100px' height='100px'</p>
+                                </div>
+                                <div class='col-sm-6'>
+                                    <h3><a style='text-decoration:none; cursor:pointer;color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+                                    <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                                </div>
+                                <div class='col-sm-4'>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col-sm-12'>
+                                    <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
+                                </div>
+                            </div><br>
+                            <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-info'>Comment</button></a><br>
+                        </div>
+                        <div class='col-sm-3'>
+
+                        </div>
+                    </div><br><br>
+                ";
+            }
+        }
+    }
 ?>
