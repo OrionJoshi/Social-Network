75a5b6132732f8d0e981974d8b79d74e377a01e1
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-08 01:56:11 -0700

    form post added

--- home.php
+++ home.php
@@ -1,5 +1,45 @@
+<!DOCTYPE html>
 <?php
     session_start();
-    echo $_SESSION['user_email'];
+    include("includes/header.php");
 
+    if(!isset($_SESSION['user_email'])){
+        header("location:index.php");
+    }
 ?>
+<html lang="en">
+<head>
+    <?php
+        $user = $_SESSION['user_email'];
+
+        $get_user = "SELECT * FROM users WHERE user_email='$user'";
+        $run_user = mysqli_query($con,$get_user);
+        $row = mysqli_fetch_array($run_user);
+        $user_name = $row['user_name'];
+    ?>
+    <title><?php echo "$user_name"; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
+</head>
+<body>
+    <div class="row">
+        <div id="insert_post" class="col-sm-12">
+            <center>
+                <form action="home.php?id=<?php echo $user_id;?>" method="post" id="f" enctype="multipart/form-data">
+                    <textarea class="form-control" name="content" id="content"  rows="4" placeholder="What's in your mind?"></textarea><br>
+                    <label for="" class="btn btn-warning" id="upload_image_button">Select Image
+                        <input type="file" name="upload_image" size="30">
+                    </label>
+                    <button id="btn-post" class="btn btn-success" name="sub">Post</button>
+                </form>
+            </center>
+        </div>
+    </div>
+
+    
+</body>
+</html>
