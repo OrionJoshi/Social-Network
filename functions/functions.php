ddfe70e1b037021b442debcf985cab55a4044ca6
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-08 04:52:02 -0700

    post added to the database

--- functions/functions.php
+++ functions/functions.php
@@ -18,6 +18,23 @@
                 echo "<script>alert('Please use 250 or less than 250 word!')</script>";
                 echo "<script>window.open('home.php','_self')</script>";
                
+            }else{
+                if(strlen($upload_image)>=1 && strlen($content)>=1){
+                    move_uploaded_file($image_tmp,"imagepost/$upload_image.$random_number");
+
+                    $insert = "INSERT INTO posts(user_id, post_content, upload_image, post_date) VALUES ('$user_id','$content','$upload_image.$random_numbe', NOW())";
+                    $run = mysqli_query($con,$insert);
+
+                    if($run){
+                        echo "<script>alert('Your Post Updated!')</script>";
+                        echo "<script>window.open('home.php','_self')</script>";
+
+                        $update = "UPDATE users SET posts = 'yes' WHERE user_id = '$user_id'";
+                        $run_update = mysqli_query($con,$update);
+               
+                    }
+                    exit();
+                }
             }
         }
     }
