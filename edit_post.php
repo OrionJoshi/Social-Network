ffd6301f466bfc2e756c2bd14c0c93d75550dd0b
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-10 05:11:06 -0700

    bootstrap and css link added

--- edit_post.php
+++ edit_post.php
@@ -11,6 +11,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
     <title>Edit Post</title>
 </head>
 <body>
