393b36bc8f1b8a3a1862fdd75c37d581cee58ffb
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-10 06:28:22 -0700

    bootstrap and css link added

---
+++ single.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    include("includes/header.php");
+
+    if(!isset($_SESSION['user_email'])){
+        header("location:index.php");
+    }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Your Post</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
+    
+</head>
+<body>
+    
+</body>
+</html>
