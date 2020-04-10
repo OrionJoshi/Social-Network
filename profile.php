6e905b9860c8fe1c3f92380854f29fc401301d5c
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-10 04:41:58 -0700

    css for button added

--- profile.php
+++ profile.php
@@ -48,6 +48,14 @@
         transform:translate(-50%,-50%);
 
     }
+    #own_posts{
+        border:5px solid #e6e6e6;
+        padding:40px 50px;
+    }#post_img{
+        height:300px;
+        width:100%;
+    }
+
 </style>
 <body>
     <div class="row">
@@ -207,8 +215,8 @@
                                         <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
                                     </div>
                                 </div><br>
-                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a><br>
-                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a><br>
+                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a>
+                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a>
 
                             </div><br><br>
                         
@@ -235,8 +243,8 @@
                                         <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
                                     </div>
                                 </div><br>
-                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a><br>
-                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a><br>
+                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a>
+                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a>
 
                             </div><br><br>
                         
@@ -258,16 +266,46 @@
                                     </div>
                                 </div>
                                 <div class='row'>
-                                    <div class='col-sm-12'>
-                                        <p>$content</p>
+                                    <div class='col-sm-2'>
                                     </div>
-                                </div><br>
-                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a><br>
-                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a><br>
-
-                            </div><br><br>
+                                    <div class='col-sm-6'>
+                                        <h3><p>$content</p></h3>
+                                    </div>
+                                    <div class='col-sm-4'></div>
+                            </div>
                         
                         ";
+
+                        global $con;
+
+                        if(isset($_GET['u_id'])){
+                            $u_id = $_GET['u_id'];
+                        }
+                        $get_posts = "SELECT user_email FROM users WHERE user_id='$u_id'";
+                        $run_user = mysqli_query($con,$get_posts);
+                        $row = mysqli_fetch_array($run_user);
+
+                        $user_email = $row['user_email'];
+
+                        $user = $_SESSION['user_email'];
+                        $get_user = "SELECT * FROM users WHERE user_email='$user'";
+                        $run_user = mysqli_query($con,$get_user);
+                        $row = mysqli_fetch_array($run_user);
+
+                        $user_id = $row['user_id'];
+                        $u_email = $row['user_email'];
+
+                        if($u_email != $user_email){
+                            echo "<script>window.open('profile.php?u_id=$user_id','_self')</script>";
+                        }else{
+                            
+                           echo "
+                                <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-success'>View</button></a>
+                                <a href='edit_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-info'>Edit</button></a>
+                                <a href='functions/delete_post.php?post_id=$post_id' style='float:right;'><button class='btn btn-danger'>Delete</button></a>
+                                </div><br><br>
+                            ";
+                        }
 
                     }
                     
