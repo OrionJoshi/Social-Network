4791f7cd78911c4a2039d48a6c307b866694e204
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-13 10:02:52 -0700

    get user id from url

---
+++ user_profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    include("includes/header.php");
+
+    if(!isset($_SESSION['user_email'])){
+        header("location:index.php");
+    }
+?>
+<html lang="en">
+<head>
+    <title>Profile</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
+</head>
+<body>
+    <div class="row">
+        <?php
+            if(isset($_GET['u_id'])){
+                $u_id = $_GET['u_id'];
+            }
+        ?>
+    </div>
+</body>
+</html>
