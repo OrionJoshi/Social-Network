8a62d6f0b1114338ff6edc48edd24ff18d24d272
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-13 10:38:28 -0700

    fetching user data from database

--- user_profile.php
+++ user_profile.php
@@ -23,7 +23,58 @@
             if(isset($_GET['u_id'])){
                 $u_id = $_GET['u_id'];
             }
+            if($u_id < 0 || $u_id == ""){
+                echo "<script>window.open('home.php','_self')</script>";
+            }else{
+
         ?>
+        <div class="col-sm-12">
+            <?php
+                if(isset($_GET['u_id'])){
+                    global $con;
+                    $user_id = $_GET['u_id'];
+
+                    $select = "SELECT * FROM users WHERE user_id='$user_id'";
+                    $run = mysqli_query($con,$select);
+                    $row = mysqli_fetch_array($run);
+
+                    $username = $row_user['user_name'];
+        
+                }
+            ?>
+            <?php
+                if(isset($_GET['u_id'])){
+                    global $con;
+
+                    $user_id = $_GET['u_id'];
+                    $select = "SELECT * FROM users WHERE user_id='$user_id'";
+                    $run = mysqli_query($con,$select);
+                    $row = mysqli_fetch_array($run);
+
+                    $id = $row['user_id'];
+                    $name = $row['user_name'];
+                    $f_name =$row['f_name'];
+                    $l_name = $row['l_name'];
+                    $describe_user = $row['describe_user'];
+                    $relationship_status = $row['relationship'];
+                    $user_email = $row['user_email'];
+                    $country = $row['user_country'];
+                    $gender = $row['user_gender'];
+                    $image = $row['user_image'];
+                    $register_date = $row['user_reg_date'];
+
+                    echo "
+                        <div class='row'>
+                            <div class='col-sm-1'></div>
+                            <center>
+                                <div style='background-color:#e6e6e6;' class='col-sm-3'></div>
+                            </center>    
+                        </div>
+                    
+                    ";
+                }
+            ?>
+        </div>
     </div>
 </body>
 </html>
