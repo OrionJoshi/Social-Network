70b3088ccd84e57f376d48780011014c900d5b8d
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-15 21:32:53 +0545

    condition added if user didnt write anything

--- edit_profile.php
+++ edit_profile.php
@@ -135,7 +135,10 @@
             <?php
                 if(isset($_POST['sub'])){
                     $bfn = htmlentities($_POST['content']);
-
+                    if($bfn == ""){
+                        echo "<script>alert('Please Enter Something!')</script>";
+                        echo "<script> window.open('edit_profile.php?u_id = $user_id','_self')</script>";
+                    }
                     
                 }
             ?>
