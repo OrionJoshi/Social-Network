cb31dda7fee68da8b2fe53b021cc245866b523ab
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-16 01:17:39 +0545

    searching posts added

--- functions/functions.php
+++ functions/functions.php
@@ -537,6 +537,95 @@
             $last_name = $row_user['l_name'];
             $user_image = $row_user['user_image'];
 
+            //Displaying Post from user search bar
+            if($content=="NO" && strlen($upload_image)>=1){
+                echo"
+                    <div class='row'>
+                        <div class='col-sm-3'>
+                        </div>
+                        <div id='posts' class='col-sm-6'>
+                            <div class='row'>
+                                <div class='col-sm-2'>
+                                    <p><img src='$user_image' class = 'img-circle' width='100px' height='100px'</p>
+                                </div>
+                                <div class='col-sm-6'>
+                                    <h3><a style='text-decoration:none; cursor:pointer;color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+                                    <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                                </div>
+                                <div class='col-sm-4'>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col-sm-12'>
+                                    <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
+                                </div>
+                            </div><br>
+                        </div>
+                        <div class='col-sm-3'>
+
+                        </div>
+                    </div><br><br>
+                ";
+            }
+            elseif (strlen($content)>=1 && strlen($upload_image)>=1) {
+                echo"
+                <div class='row'>
+                    <div class='col-sm-3'>
+                    </div>
+                    <div id='posts' class='col-sm-6'>
+                        <div class='row'>
+                            <div class='col-sm-2'>
+                                <p><img src='$user_image' class = 'img-circle' width='100px' height='100px'</p>
+                            </div>
+                            <div class='col-sm-6'>
+                                <h3><a style='text-decoration:none; cursor:pointer;color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+                                <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                            </div>
+                            <div class='col-sm-4'>
+                            </div>
+                        </div>
+                        <div class='row'>
+                            <div class='col-sm-12'>
+                                <p>$content</p>
+                                <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
+                            </div>
+                        </div><br>
+                    </div>
+                    <div class='col-sm-3'>
+
+                    </div>
+                </div><br><br>
+            ";
+            }else{
+                echo"
+                <div class='row'>
+                    <div class='col-sm-3'>
+                    </div>
+                    <div id='posts' class='col-sm-6'>
+                        <div class='row'>
+                            <div class='col-sm-2'>
+                                <p><img src='$user_image' class = 'img-circle' width='100px' height='100px'</p>
+                            </div>
+                            <div class='col-sm-6'>
+                                <h3><a style='text-decoration:none; cursor:pointer;color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+                                <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                            </div>
+                            <div class='col-sm-4'>
+                            </div>
+                        </div>
+                        <div class='row'>
+                            <div class='col-sm-12'>
+                                <h3><p>$content</p></h3>
+                            </div>
+                        </div><br>
+                    </div>
+                    <div class='col-sm-3'>
+
+                    </div>
+                </div><br><br>
+            ";
+            }    //else condition ending
+
         }
     }
     //Search user function defination
