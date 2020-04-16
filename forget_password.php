82237f1ca696f908875f2a097a11111c2e38d8cc
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-16 19:39:22 +0545

    heading added

---
+++ forget_password.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    include("includes/header.php");
+
+    if(!isset($_SESSION['user_email'])){
+        header("location:index.php");
+    }
+?>
+<html lang="en">
+<head>
+    <title>Forget Password</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
+</head>
+<body>
+    <div class="row">
+       <div class="col-sm-12">
+            <div class="well">
+                <center><h1 style="color:white;"><strong>Social Network</strong></h1></center>
+            </div>
+       </div> 
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="main-content">
+                <div class="header">
+                    <h3 style="text-align:center;"><strong>Forget Password</strong></h3><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
