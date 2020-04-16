8f132aa68d9cc9b43359efe4a8b1262c691ad3a6
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-16 20:12:20 +0545

    data taken from forget password form

--- forget_password.php
+++ forget_password.php
@@ -1,12 +1,4 @@
 <!DOCTYPE html>
-<?php
-    session_start();
-    include("includes/header.php");
-
-    if(!isset($_SESSION['user_email'])){
-        header("location:index.php");
-    }
-?>
 <html lang="en">
 <head>
     <title>Forget Password</title>
@@ -43,7 +35,7 @@
                             <span class="input-group-addon"><i clas="glyphicon glyphicon-pencil"></i></span>
                             <input type="text" class="form-control" placeholder="Someone" name="recovery_account" required>
                         </div><br>
-                        <a style="text-decoration:none; float:right;color=#187FAB;" data-toggle="tooltip" title="Signin" href="signin.php">Back To Signin ?</a>
+                        <a style="text-decoration:none; float:right;color=#187FAB;" data-toggle="tooltip" title="Signin" href="login.php">Back To Signin ?</a>
                         <center><button id="signup" class="btn btn-info btn-lg" name="submit">Submit</button></center>
                     </form>
                 </div>
@@ -52,3 +44,26 @@
     </div>
 </body>
 </html>
+<?php
+    session_start();
+    include("includes/connection.php");
+    
+    if(isset($_POST['submit'])){
+
+        $email = htmlentities(mysqli_real_escape_string($con,$_POST['email']));
+        $recovery_account = htmlentities(mysqli_real_escape_string($con,$_POST['recovery_account']));
+
+        $select_user = "SELECT * FROM users WHERE user_email = '$email' AND recovery_account='$recovery_account'";
+        $query = mysqli_query($con,$select_user);
+        $check_user = mysqli_num_rows($query);
+
+        if($check_user == 1){
+            $_SESSION['user_email'] = $email;
+
+            echo "<script>window.open('change_password.php','_self')</script>";
+        }else{
+            echo "<script>alert('Your Email or recovery input incorrect!')</script>";
+        }
+    }
+
+?>
